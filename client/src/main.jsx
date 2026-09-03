048380d55a06219fdc73f2ee1bfb86386be0a38e
Emanuele Tocci <tocciemanuele03@gmail.com>
2026-09-03 13:33:58 +0200

    main.jsx check

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,16 +11,7 @@ import App from "./App.jsx";
 // - createRoot(...) creates a React root attached to that DOM node.
 // - .render(...) tells React what to draw inside it, for the first time.
 createRoot(document.getElementById("root")).render(
-	// StrictMode: a development-only wrapper that doesn't render any visible UI.
-	// It helps catch mistakes by intentionally double-invoking some functions
-	// (like component bodies and effects) during development, to surface
-	// side effects that aren't properly cleaned up. It has no effect in production.
 	<StrictMode>
-		{/*
-      BrowserRouter: provides routing capabilities  to every component inside it.
-      This is what makes <Routes>, <Route>, <Link>, useNavigate(), etc.
-      work correctly inside <App />.
-    */}
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
